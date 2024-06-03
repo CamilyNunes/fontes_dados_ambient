dcc121e55b6851247b75ab468e42d016769ab7b3
Camily Nunes <camilynunes191@gmail.com>
2024-06-03 17:24:57 -0400

    Lista com as fontes de dados

---
+++ lista_fontes_dados.R
@@ -0,0 +1,78 @@
+#Atividade 1: Estimar a área desmatada utilizando imagens de satélite
+
+# Fazer uma lista com as fontes de dados de desmatamento
+
+1. Global Forest Watch (GFW):https://www.globalforestwatch.org/
+  Descrição: Plataforma interativa que fornece dados sobre florestas em tempo real.
+
+#Importando os dados do Brasil
+
+- Em processo
+- Dados robustos
+
+2.MODIS (Moderate Resolution Imaging Spectroradiometer):
+  
+  Site: https://modis.gsfc.nasa.gov/
+  Descrição: Dados de desmatamento obtidos por satélites da NASA.
+- Há informações interresantes sobre os oceanos, mas para usar os seus dados a NASA tem o seu próprio pacote e diversos pré-requisitos.
+- Tem dados muito abrangente.
+
+3. FAO Global Forest Resources Assessment (FRA):
+  
+  Site: https://oceancolor.gsfc.nasa.gov/data/use-data/
+  Descrição: 
+  - Avaliação global dos recursos florestais realizada pela Organização das Nações Unidas para a Alimentação e a Agricultura.
+- A Avaliação Global dos Recursos Florestais (FRA) da FAO fornece informações essenciais para a compreensão da extensão dos recursos florestais, sua condição, gestão e usos.
+-A avaliação em curso, FRA 2025, examina a situação e as tendências de mais de 60 variáveis relacionadas com as florestas em 236 países e territórios no período 1990-2025. 
+- Os dados FRA 2025 são recolhidos utilizando termos e definições comummente acordados através de um processo de comunicação transparente e rastreável e de uma rede bem estabelecida de correspondentes nacionais oficialmente nomeados que abrange 191 países e territórios.
+
+#Dados robusto#
+
+## Fontes Nacionais (Brasil)
+
+4.INPE (Instituto Nacional de Pesquisas Espaciais):
+  
+  INPE (Instituto Nacional de Pesquisas Espaciais):
+  Descrição: O INPE oferece diversos programas de monitoramento por satélite do desmatamento na Amazônia e outras regiões do Brasil.
+Programas: PRODES, DETER, TerraClass
+
+5.IBGE (Instituto Brasileiro de Geografia e Estatística):
+  
+  Descrição: Dados estatísticos sobre o uso da terra e cobertura florestal no Brasil.
+Obs.: #Não achei de forma rápida os dados#
+  
+  ##Organizações Não Governamentais (ONGs)
+  Observação: A partir daqui são apenas empresas que utiliza dados sobre a degradação ambiental.
+
+6. WWF (World Wide Fund for Nature):
+  
+  Site: WWF
+Descrição: Relatórios e dados sobre desmatamento e conservação de florestas.
+Obs.: Não encontrei dados que podem ser útilizados para mapeamento de degradação.
+
+7.Greenpeace:
+  
+  Site: Greenpeace
+Descrição: Dados e campanhas sobre desmatamento e proteção de florestas.
+Universidades e Centros de Pesquisa
+
+8.Universidade de Maryland:
+  
+  Site: Global Land Analysis & Discovery (GLAD)
+Descrição: Fornece dados e análises sobre a cobertura florestal global.
+
+9. CIFOR (Center for International Forestry Research):
+  
+  Site: CIFOR
+Descrição: Pesquisa e dados sobre a gestão sustentável das florestas tropicais.
+Empresas Privadas
+
+10. Planet Labs:
+  
+  Site: Planet Labs
+Descrição: Imagens de satélite de alta resolução para monitoramento de mudanças ambientais, incluindo desmatamento.
+
+11.Google Earth Engine:
+  
+  Site: Google Earth Engine
+Descrição: Plataforma para análise de dados geoespaciais em escala global, utilizada para monitoramento de desmatamento
